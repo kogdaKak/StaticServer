3de38eaddf85e24772370c373efbf19ccd4e5d6d
KogdaKak <nub.n.2015@mail.ru>
2025-11-28 00:30:21 +0300

    First load files

---
+++ Server/Logic/ReaderWriterCount.cs
@@ -0,0 +1,53 @@
+﻿using Server.Interfaces;
+using System.Threading;
+
+namespace Server.Logic
+{
+    public class ReaderWriterCount : ICounter
+    {
+        private int _count;
+        private ReaderWriterLockSlim _lock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+
+        public int GetCount()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public void AddToCount(int value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                checked
+                {
+                    _count += value;
+                }
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        public void Reset()
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _count = 0;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+}
---
+++ Server/Server/LockServer.cs
@@ -0,0 +1,18 @@
+﻿using Server.Interfaces;
+using Server.Logic;
+
+namespace Server.Server
+{
+    public static class LockServer
+    {
+        private static readonly ICounter _counter = new ReaderWriterCount();
+        public static int GetCount() => _counter.GetCount();
+        public static void AddToCount(int value) => _counter.AddToCount(value);
+
+        public static void ResetForTest()
+        {
+            if (_counter is ReaderWriterCount impl)
+                impl.Reset();
+        }
+    }
+}
